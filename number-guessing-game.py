fab0a318c1de17f46b251672cd116c5f9d26b531
Amir <amir.79.ahz@gmail.com>
2023-08-14 22:47:13 +0330

    I added comments for better understanding.

--- number-guessing-game.py
+++ number-guessing-game.py
@@ -4,13 +4,16 @@ import random
 lower = int(input("Enter the lower number: "))
 upper = int(input("Enter the upper number: "))
 
+#generating the numbers
 x = random.randint(lower, upper)
 print("\n\tYou have only ",
       round(math.log(upper - lower + 1, 2)),
       " chances to guess the number!\n")
 
+#initializing the number of guesses
 count = 0
 
+#calculating of minimum of guesses depends upon range
 while count < math.log(upper - lower + 1, 2):
     count += 1
 
